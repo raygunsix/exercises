94bb48c3771369f5046b3a835a6b6ed219570602
Chris Reid <chris@raygun6.com>
2024-11-19 16:28:57 -0800

    add exit button

--- converter.py
+++ converter.py
@@ -17,24 +17,31 @@ inches_label = sg.Text("Enter inches:")
 inches_input_box = sg.InputText(tooltip="Enter inches", key="inches")
 
 convert_button = sg.Button("Convert")
+exit_button = sg.Button("Exit")
 status_text = sg.Text("", key="status")
 
 window = sg.Window('Converter', layout=[[feet_label, feet_input_box],
                                         [inches_label, inches_input_box],
-                                        [convert_button, status_text]])
+                                        [convert_button, exit_button, status_text]])
 
 while True:
     event, values = window.read()
     print("Event: " + str(event))
     print("Values: " + str(values))
 
-    inches = float(values['inches'])
-    feet = float(values['feet'])
+    match event:
+        case "Convert":  
+            inches = float(values['inches'])
+            feet = float(values['feet'])
 
-    meters = convert(feet, inches)
+            meters = convert(feet, inches)
 
-    message = "Meters: " + str(round(meters, 3)) + " m"
-    window["status"].update(value=message)
-    print(message)
+            message = "Meters: " + str(round(meters, 3)) + " m"
+            window["status"].update(value=message)
+            print(message)
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break   
 
 window.close()
