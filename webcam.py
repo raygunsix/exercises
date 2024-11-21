abadfdd768c9193943821495320d462b9b0ba364
Chris Reid <chris@raygun6.com>
2024-11-21 14:02:09 -0800

    enable images to be uploaded

--- webcam.py
+++ webcam.py
@@ -1,13 +1,27 @@
 # Note: This script runs only on a local IDE with "streamlit run main.py"
 import streamlit as st
 from PIL import Image
- 
+
+def convert_to_grayscale(image):
+    """ converts a image to grayscale """
+    img = Image.open(image)
+    graysacle_img = img.convert('L')
+    return graysacle_img
+
+
 st.subheader("Color to Grayscale Converter")
- 
+
+uploaded_image = st.file_uploader("Upload Image")
+
 with st.expander("Start camera"):
     camera_image = st.camera_input("Camera")
- 
+
 if camera_image:
-    img = Image.open(camera_image)
-    gray_camera_img = img.convert('L')
-    st.image(gray_camera_img)
+    grayscale_img = convert_to_grayscale(camera_image)
+    st.image(grayscale_img)
+
+if uploaded_image:
+    grayscale_img = convert_to_grayscale(uploaded_image)
+    st.image(grayscale_img)    
+
+# st.session_state
