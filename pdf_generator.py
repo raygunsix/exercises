19d9ce941b15fa88ca7e9ebed40da274492272fb
Chris Reid <chris@raygun6.com>
2024-11-26 15:49:22 -0800

    read animal from data file name

--- pdf_generator.py
+++ pdf_generator.py
@@ -1,2 +1,9 @@
 import glob
 from fpdf import FPDF
+from pathlib import Path
+
+filepaths = glob.glob("data-animals/*.txt")
+
+for filepath in filepaths:
+    animal = Path(filepath).stem
+    print(animal)
