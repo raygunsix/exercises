461461f5d735c1677eaa96e5761d068df07ac4a7
Chris Reid <chris@raygun6.com>
2024-11-26 16:00:42 -0800

    add 1 page per animal

--- pdf_generator.py
+++ pdf_generator.py
@@ -1,9 +1,17 @@
 import glob
-from fpdf import FPDF
 from pathlib import Path
+from fpdf import FPDF
+
 
 filepaths = glob.glob("data-animals/*.txt")
+pdf = FPDF(orientation="portrait", unit="mm", format="A4")
 
 for filepath in filepaths:
     animal = Path(filepath).stem
-    print(animal)
+    
+    pdf.add_page()
+
+    pdf.set_font(family="Times", style="B", size=18)
+    pdf.cell(w=50, h=8, txt=animal.title(), ln=1)
+
+pdf.output("output/output.pdf")
