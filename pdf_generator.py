d35593b761a97e2179173b3251a8e34aedd01d82
Chris Reid <chris@raygun6.com>
2024-11-27 15:31:15 -0800

    add text content to each page

--- pdf_generator.py
+++ pdf_generator.py
@@ -10,9 +10,17 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     title = filename.capitalize()
 
+    with open(filepath, 'r') as file:
+        content = file.read()
+    
     pdf.add_page()
 
+    # Add title
     pdf.set_font(family="Times", style="B", size=18)
     pdf.cell(w=50, h=8, txt=title, ln=1)
+
+    # Add body
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
 
 pdf.output("output/output.pdf")
