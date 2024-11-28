b8efdfaf581c76d8c30ec8ade7f1e6aa6fedb5d2
Chris Reid <chris@raygun6.com>
2024-11-28 15:30:56 -0800

    add initial api request

--- astronomy_picture.py
+++ astronomy_picture.py
@@ -1,6 +1,20 @@
+import os
+import requests
 import streamlit as st
 
+api_key = os.getenv("NASA_API_KEY")
+url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
+
 st.set_page_config(layout="centered")
+
+# Fetch APOD data from NASA API
+# https://api.nasa.gov/
+r = requests.get(url)
+
+print(r)
+
+# Render web page
+
 
 st.title("Title")
 
