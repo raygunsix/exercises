a73cf23f11fc74519eb91c8818721c49c73521c5
Chris Reid <chris@raygun6.com>
2024-11-29 18:05:37 -0800

    add initial dictionary api endpoint

--- dictionary-api.py
+++ dictionary-api.py
@@ -1,2 +1,13 @@
 from flask import Flask
 
+app =  Flask(__name__)
+
+@app.route("/api/v1/<word>")
+def api(word):
+    definition = word.upper()
+    return {
+        "definition": definition,
+        "word": word
+    }
+
+app.run(debug=True, port=5001)
