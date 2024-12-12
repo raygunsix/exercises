43291162f660b2dd61b8d9422eddd1218746890f
Chris Reid <chris@raygun6.com>
2024-12-12 13:15:01 -0800

    implement get name method

--- oop-sample.py
+++ oop-sample.py
@@ -5,12 +5,16 @@ class User:
         self.birthyear = birthyear
 
     def get_name(self):
-        pass
+        name_all_caps = self.name.upper()
+        return name_all_caps
 
     def age(self, current_year):
         age = current_year - self.birthyear
         return age
 
+
 bob = User("bob", 1999)
 
 print(bob.age(2023))
+
+print(bob.get_name())
