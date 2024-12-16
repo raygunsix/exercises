ec8e92434f41644554da798d2956a649ef56c5f1
Chris Reid <chris@raygun6.com>
2024-12-16 15:39:00 -0800

    add combo box widget for unit selection

--- speed-calc-gui.py
+++ speed-calc-gui.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton
+from PyQt6.QtWidgets import QApplication, QComboBox, QGridLayout, QLabel, QLineEdit, QPushButton, QWidget 
 
 
 class SpeedCalculator(QWidget):
@@ -10,14 +10,22 @@ class SpeedCalculator(QWidget):
 
         # Create widgets
         distance_label = QLabel("Distance: ")
+        distance_line_edit = QLineEdit()
 
         time_label = QLabel("Time (hours): ")
+        time_line_edit = QLineEdit()
 
-        calculate_button = QPushButton("Calculatre Speed: ")
+        combo = QComboBox()
+        combo.addItems(['Metric (km)', 'Imperial (miles)'])
+
+        calculate_button = QPushButton("Calculate")
 
         # Add widgets to greid
         grid.addWidget(distance_label, 0, 0)
-        grid.addWidget(time_label, 0, 1)
+        grid.addWidget(distance_line_edit, 0, 1)
+        grid.addWidget(combo, 0, 2)
+        grid.addWidget(time_label, 1, 0)
+        grid.addWidget(time_line_edit, 1, 1)
         grid.addWidget(calculate_button, 2, 0, 1, 2)
 
         self.setLayout(grid)
